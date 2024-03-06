54b5a0dea77530c1c75d4243e03ad774f66bfb5c
itsaswinravi <achu322001@gmail.com>
2024-03-06 15:39:41 +0530

    done

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,7 +1,14 @@
-import React, {useState} from 'react'
+import React, {useEffect,useState} from 'react'
 import Create from './Create'
+import axios from 'axios'
 function Home() {
   const[todos,settodos]=useState([])
+  useEffect(()=>
+  {
+    axios.get('http://localhost:3001/get')
+    .then(result => settodos(result.data))
+    .catch(err => console.log(err))
+  },[])
   return (
     <div className='Home'>
       <h2>todo list</h2>
@@ -12,7 +19,7 @@ function Home() {
         :
         todos.map(todo => (
           <div>
-            {todo}
+            {todo. task }
 
           </div>
           
